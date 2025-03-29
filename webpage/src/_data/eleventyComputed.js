6d6ae20dfa5cc0970040fcc87a895f00bbc7acfb
Denperidge <denperidge@gmail.com>
2025-03-29 19:33:03 +0100

    [web] Data structure change, added index

---
+++ webpage/src/_data/eleventyComputed.js
@@ -0,0 +1,20 @@
+export default {
+    allAbout: (data) => {
+        return Array.from(
+            new Set(
+                data.fictionalTestData.map(entry => entry.about)
+            )
+        );
+    },
+    ratingsById: (data) => {
+        const out = {};
+        data.fictionalTestData.forEach((entry) => {
+            const { about, rating, comment } = entry; 
+            if (!Object.keys(out).includes(about)) {
+                out[about] = [];
+            }
+            out[about].push({rating, comment})
+        })
+        return out;
+    }
+}
