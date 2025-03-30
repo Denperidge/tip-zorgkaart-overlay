7073732c8f2c5fe8a5b63e674f080aa152262cf0
Denperidge <denperidge@gmail.com>
2025-03-30 23:09:56 +0200

    [web] Implemented pico, updated styling

--- webpage/.eleventy.js
+++ webpage/.eleventy.js
@@ -1,7 +1,10 @@
 import eleventyAutoCacheBuster from "eleventy-auto-cache-buster";
 import eleventySass from "@grimlink/eleventy-plugin-sass";
-import sass from "sass";
+import * as sass from "sass";
 import ratings from "./src/_data/fictionalTestData.json" with { type: "json" };
+
+const PICO_VERSION = "node_modules/@picocss/pico/css/pico.fuchsia.css";
+
 
 const ratingsByAbout = {};
 ratings.forEach(({about, rating, comment}) => {
@@ -23,6 +26,8 @@ export default function (eleventyConfig) {
     eleventyConfig.addGlobalData("ratingsByAbout", ratingsByAbout)
     eleventyConfig.addPlugin(eleventyAutoCacheBuster);
 
+    eleventyConfig.addGlobalData("picoVersion", PICO_VERSION)
+    eleventyConfig.addPassthroughCopy(PICO_VERSION)
     eleventyConfig.addPlugin(eleventySass, {
         sass: sass,
         outputPath: "",
