3cde123a4a7a71c300459b302ab2746b41bfee16
Denperidge <denperidge@gmail.com>
2025-04-09 15:57:10 +0200

    [ext] sessionStorage cache for list_page

--- browser-extension/list_page.js
+++ browser-extension/list_page.js
@@ -1,23 +1,37 @@
+function addRatingBadges(summaryRatings) {
+    const ids = Object.keys(summaryRatings)
+    ids.forEach((id) => {
+        const target = document.querySelector(`.teaser:has(a[href$="/${id}"]) .teaser-author`);
+        if (target) {
+            const p =document.createElement("span");
+            p.classList.add("rating-badge")
+            p.ariaLabel = "TIP Zorgkaart Overlay rating"
+            p.innerText = `Rating: ${summaryRatings[id]}`
+            target.after(p)
+        }
+    })
+}
+
+
 async function init() {
-    fetch("https://tipzo.neonpastel.net/summary.json")
-        .then(async (res) => {
-            const ratings = await res.json();
-            const ids = Object.keys(ratings)
-            ids.forEach((id) => {
-                const target = document.querySelector(`.teaser:has(a[href$="/${id}"]) .teaser-author`);
-                if (target) {
-                    console.log("target");
-                    const p =document.createElement("span");
-                    p.classList.add("rating-badge")
-                    p.ariaLabel = "TIP Zorgkaart Overlay rating"
-                    p.innerText = `Rating: ${ratings[id]}`
-                    target.after(p)
-                }
+    let summary = sessionStorage.getItem("summary")
+    if (summary) {
+        console.log("Summary from storage...");
+        addRatingBadges(JSON.parse(summary));
+    } else {        
+        console.log("Summary from tipzo...")
+        fetch("https://tipzo.neonpastel.net/summary.json")
+            .then(async (res) => {
+                const ratings = await res.json();
+                sessionStorage.setItem("summary", JSON.stringify(ratings));
+                addRatingBadges(ratings)    
             })
-        })
-        .catch((err) => {
-            if (err) { console.error(err); }
-        })
+            .catch((err) => {
+                if (err) { console.error(err); }
+            })
+            
+    }
+
 }
 
 init();
