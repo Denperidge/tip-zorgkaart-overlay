7fc11699395f388d6d81642c4a7d2cba28dfb263
Denperidge <denperidge@gmail.com>
2025-04-09 18:39:23 +0200

    [web/ext] map page JS-based rating badges

--- browser-extension/page_map.js
+++ browser-extension/page_map.js
@@ -1,14 +1,22 @@
-let timeout = undefined;
+let summary;
 
 function showInfoWindowRating() {
-    const selectedLocation = document.querySelector(".info-window-map");
-    if (selectedLocation) {
-        alert("meow")
+    const selectedProvider = document.querySelector(".info-window-map a");
+    if (selectedProvider && !document.querySelector(".info-window-map .rating-badge")) {
+        const href = selectedProvider.getAttribute("href");
+        const id = href.substring(href.lastIndexOf("/") + 1);
         
+        const p = document.createElement("span");
+        p.classList.add("rating-badge")
+        p.ariaLabel = "TIP Zorgkaart Overlay rating"
+        p.innerText = `Rating: ${summary[id]}`
+        document.querySelector(".info-window-map__txt").after(p)
     }
 }
 
 function init() {
+    summaryFromCache(data => {summary = data;});
+
     const observer = new MutationObserver((mutations, observer) => {
         mutations.forEach(mutation => {
             if(mutation.target.getAttribute("class") === "gm-style-iw-a") {
@@ -18,6 +26,5 @@ function init() {
 
     });
     observer.observe(document.querySelector(".map-list"), {subtree: true, childList: true})
-
 }
 init();
